432040365d9e49d993e4e6a744eed69a877441e4
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2023-09-20 15:58:51 +0100

    Created a seperate messages.py file for easier message changes.

--- jamie.py
+++ jamie.py
@@ -7,6 +7,7 @@ import os
 import asyncio
 import json
 import random
+from messages import LEVEL_UP_MESSAGES, NEW_ROLE_MESSAGES
 
 # Load environment variables from .env file
 load_dotenv()
@@ -22,37 +23,6 @@ ROLE_LVL25_ID = int(os.getenv("ROLE_LVL25_ID"))
 ROLE_LVL50_ID = int(os.getenv("ROLE_LVL50_ID"))
 ROLE_LVL100_ID = int(os.getenv("ROLE_LVL100_ID"))
 ROLE_LVL500_ID = int(os.getenv("ROLE_LVL500_ID"))
-
-LEVEL_UP_MESSAGES=[
-    "Hey there, {message.author.mention}. You've achieved level {{user_data[author_id]['level']}}! Keep up the fantastic work on your journey to better wellbeing.",
-    "Bravo, {message.author.mention}. You're now at level {{user_data[author_id]['level']}}! Your dedication to self-care and happiness is shining through.",
-    "Well done, {message.author.mention}. Level {{user_data[author_id]['level']}} unlocked! Your commitment to personal wellness is truly inspiring.",
-    "Look who's climbing the ranks, {message.author.mention}. You've reached level {{user_data[author_id]['level']}}! Keep up the positivity and self-improvement.",
-    "Ding, ding, ding, {message.author.mention}. You've made it to level {{user_data[author_id]['level']}}! Your wellbeing journey is definitely on the right track.",
-    "Hooray, {message.author.mention}! You have reached level {{user_data[author_id]['level']}}! Keep up with those good vibes.",
-    "Way to go, {message.author.mention}. You're now at level {{user_data[author_id]['level']}}! Your focus on wellbeing is paying off in spades.",
-    "Achievement unlocked, {message.author.mention}! You're now at level {{user_data[author_id]['level']}}! Continue to prioritise your health and happiness.",
-    "You're on fire, {message.author.mention}. Level {{user_data[author_id]['level']}} is yours! Keep nurturing your wellbeing and thriving.",
-    "Level up, level up, {message.author.mention}! You've achieved level {{user_data[author_id]['level']}}! The journey toward improved wellbeing continues and you're doing fantastic."
-]
-
-NEW_ROLE_MESSAGES=[
-    "Outstanding work, {message.author.mention}! Your consistent dedication and contributions to our wellbeing community have earned you the {role.name} role. Continue to enrich our community while nurturing your own growth and wellbeing.",
-    "Well-earned, {message.author.mention}! Your reputation for being a caring, committed, and encouraging member of our community has granted you the {role.name} role. Keep uplifting others and discover even more about your own journey to wellbeing.",
-    "Congratulations, {message.author.mention}! Your standing as a reliable and respectful member of our wellbeing community has unlocked the {role.name} role. Keep guiding your fellow members and deepen your understanding of wellbeing, benefiting both yourself and the community.",
-    "A significant milestone, {message.author.mention}! Your unwavering commitment and respect for others has led you to the {role.name} role. Continue to foster belonging, offer support, and embark on a shared path of growth.",
-    "Bravo, {message.author.mention}! Your reputation as a supportive and considerate community member has earned you the {role.name} role. Inspire others and discover fresh perspectives on your own wellbeing journey.",
-    "Achievement unlocked, {message.author.mention}! The {role.name} role is yours, a testament to your outstanding reputation, respect, and dedication to our community. Make an enduring positive impact on our members while progressing personally.",
-    "Amazing, {message.author.mention}! Your unwavering dedication, respect, and contributions to our wellbeing community have led to the achievement of the {role.name} role. You enhance wellbeing for us all.",
-    "Fantastic news, {message.author.mention}! Your ongoing commitment, respect for others, and dedication to our community has resulted in you attaining the {role.name} role. Keep pushing forward, lend a helping hand, and continue your journey of self-improvement and community support.",
-    "Impressive work, {message.author.mention}! Your consistent dedication and contributions to our wellbeing community has earned you the {role.name} role. Embrace this opportunity to continue enriching our community with your knowledge and support, while also nurturing your personal growth.",
-    "Well-deserved, {message.author.mention}! Your reputation for being a caring, committed, and encouraging member of our community has granted you the {role.name} role. Keep setting a positive example and nurturing the growth of others, as well as your own.",
-    "Congratulations, {message.author.mention}! Your standing as a reliable and respectful member of our wellbeing community has unlocked the {role.name} role. Your wisdom and guidance can benefit fellow members on their journey and further enhance your own understanding of wellbeing.",
-    "A significant achievement, {message.author.mention}! Your unwavering commitment and respect for others have led you to the {role.name} role. Continue to foster a sense of belonging and offer your support to those in need, all while continuing to grow personally.",
-    "Bravo, {message.author.mention}! Your reputation as a supportive and considerate community member has earned you the {role.name} role. Be a beacon of positivity and encouragement to inspire others, while also finding inspiration and growth within yourself.",
-    "Well done, {message.author.mention}! Your dedication and efforts to promote respect within our community is off the charts, granting you the {role.name} role. Continue to cultivate a warm and inclusive atmosphere, benefitting both our community and yourself.",
-    "Role unlocked, {message.author.mention}! The {role.name} role is yours, a testament to your outstanding reputation, respect, and dedication to our community. Use your influence to make a lasting positive impact on our members and foster personal growth."
-]
 
 # Define your time zone (example: GMT)
 TIMEZONE = timezone('GMT')
